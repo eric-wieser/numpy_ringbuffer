42cffb3c4953bfce39dd1042553654050775dbf1
Eric Wieser <wieser.eric@gmail.com>
2016-11-14 14:15:59 +0000

    Correct license

--- setup.py
+++ setup.py
@@ -2,21 +2,23 @@ from setuptools import setup, find_packages
 setup(
     name="numpy_ringbuffer",
     version="0.1",
-    packages=find_packages(),
+    py_modules=['numpy_ringbuffer'],
 
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
     install_requires=["numpy"],
 
-    # metadata for upload to PyPI
     author="Eric Wieser",
     author_email="wieser.eric+numpy@gmail.com",
     description="Ring buffer implementation for numpy",
-    license="PSF",
+    license="MIT",
     keywords="hello world example examples",
     url="https://github.com/eric-wieser/raven-client",
     download_url="https://github.com/eric-wieser/raven-client/tarball/0.6",
-
-
-    # could also include long_description, download_url, classifiers, etc.
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5"
+    ]
 )
