558d121726906cfddf4afa86e9a1b7f2d44fcb40
Eric Wieser <wieser.eric@gmail.com>
2022-06-28 19:35:37 +0000

    use the native markdown support in setuptools

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
+from io import open
 from setuptools import setup
 
-with open('numpy_ringbuffer/__about__.py') as f:
+with open('numpy_ringbuffer/__about__.py', encoding='utf8') as f:
     exec(f.read())
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError):
-    print("No")
-    long_description = open('README.md').read()
+with open('README.md', encoding='utf8') as f:
+    long_description = f.read()
 
 setup(
     name="numpy_ringbuffer",
@@ -21,6 +18,7 @@ setup(
     author_email="wieser.eric+numpy@gmail.com",
     description="Ring buffer implementation for numpy",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     license="MIT",
     keywords=["numpy", "buffer", "ringbuffer", "circular buffer"],
     url="https://github.com/eric-wieser/numpy_ringbuffer",
@@ -31,6 +29,10 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ]
 )
